55fe74e4d7105e918af15e0ce6df0a3e3f18f2a5
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-13 11:45:10 -0300

    Simple cli-app with -v and -h

---
+++ Doc/cli_app.cr
@@ -0,0 +1,33 @@
+require "option_parser"
+
+the_beatles = [
+    "John Lennon",
+    "Paul McCartney",
+    "George Harrison",
+    "Ringo Starr"
+]
+shout = false
+OptionParser.parse do |parser|
+    parser.banner = "Welcome to The Beatles App"
+    parser.on "-v", "--version", "Show version" do
+        puts "version 0.1"
+        exit
+    end
+    parser.on "-h", "--help", "Show help" do
+        puts parser
+        exit
+    end
+    parser.on "-t", "--twist", "Twist and SHOUT" do
+        shout = true
+    end
+end
+
+members = the_beatles
+members = the_beatles.map &.upcase if shout
+
+puts ""
+puts "Group members:"
+puts "=============="
+members.each do |member|
+  puts member
+end
