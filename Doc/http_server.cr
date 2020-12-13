70dad6b669e61b7703e9bb46078a1c367a35b6e3
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-13 11:37:29 -0300

    Simple bugs fixing(quotes and HPPT to HTTP)

--- Doc/http_server.cr
+++ Doc/http_server.cr
@@ -1,6 +1,6 @@
-require 'http/server'
+require "http/server"
 
-server = HPPT::Server.new do |context|
+server = HTTP::Server.new do |context|
     context.response.content_type = "text/plain"
     context.response.print "Hello Word! The time is #{Time.local}"
 end
